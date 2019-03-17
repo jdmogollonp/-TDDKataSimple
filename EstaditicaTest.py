40c3bba151be95914b736746a5a374a5e94fe040
NATHALIA ALVAREZ <nathalia.alvarez@globant.com>
2019-03-17 13:11:08 -0500

    Paso 2-Se agrega prueba para probar con un numero (Rojo)

--- EstaditicaTest.py
+++ EstaditicaTest.py
@@ -4,3 +4,6 @@ import Estadistica
 class EstaditicaTest(TestCase):
     def test_procesar_secuencia(self):
         self.assertEqual(Estadistica().procesar_secuencia(""),[0],"Cadena vacio")
+
+    def test_procesar_secuencia_unacadena(self):
+        self.assertEqual(Estadistica().procesar_secuencia("1"), [0], "Un numero")
