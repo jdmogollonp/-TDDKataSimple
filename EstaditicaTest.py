718fc6696447c8588ecc7b3cd67ef0ca2f187be2
NATHALIA ALVAREZ <nathalia.alvarez@globant.com>
2019-03-17 18:56:22 -0500

    Paso 11-Iteracion 3-Se agrega un test para probar el caso String con dos numeros (Rojo)

--- EstaditicaTest.py
+++ EstaditicaTest.py
@@ -9,7 +9,7 @@ class EstaditicaTest(TestCase):
         self.assertEqual(Estadistica().procesar_secuencia("2"), [1, 2, 2], "Un numero")
 
     def test_procesar_secuencia_cadenaConDosNumeros(self):
-        self.assertEqual(Estadistica().procesar_secuencia("1,3"), [2, 1], "Dos numeros")
+        self.assertEqual(Estadistica().procesar_secuencia("1,3"), [2, 1, 3], "Dos numeros")
 
     def test_procesar_secuencia_cadenaConMultiplesNumeros(self):
         self.assertEqual(Estadistica().procesar_secuencia("5,2,4,7"), [4, 2], "Multiples numeros")
