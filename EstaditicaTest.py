f85d5ba8ead75d25ae180dc009adb896d844f1a3
NATHALIA ALVAREZ <nathalia.alvarez@globant.com>
2019-03-17 13:43:15 -0500

    Paso 5-Iteracion 2-Se crea el test para cuando la cadena es vacia (Rojo)

--- EstaditicaTest.py
+++ EstaditicaTest.py
@@ -3,7 +3,7 @@ from Estadistica import Estadistica
 
 class EstaditicaTest(TestCase):
     def test_procesar_secuencia(self):
-        self.assertEqual(Estadistica().procesar_secuencia(""),[0],"Cadena vacio")
+        self.assertEqual(Estadistica().procesar_secuencia(""),[0, None],"Cadena vacio")
 
     def test_procesar_secuencia_cadenaConUnNumero(self):
         self.assertEqual(Estadistica().procesar_secuencia("1"), [1], "Un numero")
