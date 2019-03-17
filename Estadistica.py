12bbf20eb13a78953fbf6fc5c0364d67b3903d1d
NATHALIA ALVAREZ <nathalia.alvarez@globant.com>
2019-03-17 18:49:31 -0500

    Paso 9-Iteracion 3-Se corrige el metodo sumar para que pase la prueba (Verde)

--- Estadistica.py
+++ Estadistica.py
@@ -3,7 +3,7 @@ __author__ = "assistant"
 class Estadistica:
     def procesar_secuencia(self,cadena):
         if cadena == "":
-            return [0, None]
+            return [0, None, None]
         elif "," in cadena:
             numeros = cadena.split(",")
             return [len(numeros), int(min(numeros))]
