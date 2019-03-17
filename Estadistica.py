e30c98bfaf0e918df01801d61b040e80eba605a6
NATHALIA ALVAREZ <nathalia.alvarez@globant.com>
2019-03-17 13:44:44 -0500

    Paso 5-Iteracion 2-Se corrige el metodo procesar_secuencia para que pase la prueba (Verde)

--- Estadistica.py
+++ Estadistica.py
@@ -3,7 +3,7 @@ __author__ = "assistant"
 class Estadistica:
     def procesar_secuencia(self,cadena):
         if cadena == "":
-            return [0]
+            return [0, None]
         elif "," in cadena:
             numeros = cadena.split(",")
             return [len(numeros)]
